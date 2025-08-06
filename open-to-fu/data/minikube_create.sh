a3016787506405f9199fec2c939d1b5eaed3c270
Anderson Babinski <andeerlb@gmail.com>
2025-08-06 09:46:59 -0300

    fixed script

--- open-to-fu/data/minikube_create.sh
+++ open-to-fu/data/minikube_create.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 set -e # stop the script if something wrong
 
+# awk: text processing tool
+# '{print $2}': to print the second field
+
 STATUS=$(minikube status | grep 'host:' | awk '{print $2}')
 
-if [ "$STATUS" = "Running" ]; then
-  echo "Minikube is already running."
-else
-  echo "Starting Minikube in background..."
-  minikube start --driver=docker --alsologtostderr &
+echo "Starting Minikube in background..."
+minikube start --driver=docker --alsologtostderr &
 
-  while true; do
-    sleep 5
-    STATUS=$(minikube status | grep 'host:' | awk '{print $2}')
-    CONTEXT_EXISTS=$(kubectl config get-contexts -o name | grep -w minikube || true)
-    if [ "$STATUS" = "Running" ] && [ "$CONTEXT_EXISTS" = "minikube" ]; then
-      echo "Minikube started and context exists."
-      break
-    else
-      echo "Waiting for Minikube and kubeconfig context..."
-    fi
-  done
-fi
+while true; do
+  sleep 5
+  STATUS=$(minikube status | grep 'host:' | awk '{print $2}')
+  CONTEXT_EXISTS=$(kubectl config get-contexts -o name | grep -w minikube || true)
+  if [ "$STATUS" = "Running" ] && [ "$CONTEXT_EXISTS" = "minikube" ]; then
+    echo "Minikube started and context exists."
+    break
+  else
+    echo "Waiting for Minikube and kubeconfig context..."
+  fi
+done
 
 echo "Setting kubectl context to minikube..."
 kubectl config use-context minikube
--- open-to-fu/data/setup.sh
+++ open-to-fu/data/setup.sh
@@ -15,30 +15,3 @@ if ! command -v helm &> /dev/null; then
   echo "Helm is not installed."
   exit 1
 fi
-
-# awk: text processing tool
-# '{print $2}': to print the second field
-
-STATUS=$(minikube status | grep 'host:' | awk '{print $2}')
-
-if [ "$STATUS" = "Running" ]; then
-  echo "Minikube is already running."
-else
-  echo "Starting Minikube in background..."
-  minikube start --driver=docker --alsologtostderr &
-
-  while true; do
-    sleep 5
-    STATUS=$(minikube status | grep 'host:' | awk '{print $2}')
-    CONTEXT_EXISTS=$(kubectl config get-contexts -o name | grep -w minikube || true)
-    if [ "$STATUS" = "Running" ] && [ "$CONTEXT_EXISTS" = "minikube" ]; then
-      echo "Minikube started and context exists."
-      break
-    else
-      echo "Waiting for Minikube and kubeconfig context..."
-    fi
-  done
-fi
-
-echo "Setting kubectl context to minikube..."
-kubectl config use-context minikube
