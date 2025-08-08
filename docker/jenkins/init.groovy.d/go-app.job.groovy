227cd99dd86ef5f01911d49031186d3b197255d9
Anderson Babinski <andeerlb@gmail.com>
2025-08-08 08:24:50 -0300

    changed jenkins jobs to use groovy script

---
+++ docker/jenkins/init.groovy.d/go-app.job.groovy
@@ -0,0 +1,49 @@
+import jenkins.model.*
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
+import hudson.plugins.git.*
+import hudson.scm.SCM
+import hudson.model.ParametersDefinitionProperty
+import hudson.model.ChoiceParameterDefinition
+
+def jenkins = Jenkins.getInstance()
+def jobName = "go-app"
+def prefix = "==> "
+
+println ""
+println ""
+
+if (jenkins.getItem(jobName) != null) {
+    println "${prefix} Job '${jobName}' already exists, nothing to do."
+    return
+}
+
+println "${prefix} Criating job '${jobName}'..."
+
+def gitSCM = new GitSCM(
+    GitSCM.createRepoList("https://github.com/andeerlb/devops-mug-amd-dream", null),
+    [new BranchSpec("*/master")],
+    false,
+    Collections.emptyList(),
+    null,
+    null,
+    new ArrayList<>()
+)
+
+def definition = new CpsScmFlowDefinition(gitSCM, "app/Jenkinsfile")
+definition.setLightweight(true)
+
+def job = new WorkflowJob(jenkins, jobName)
+job.setDefinition(definition)
+
+def choices = ["dev", "prod"]
+def param = new ChoiceParameterDefinition("env", choices as String[], "Choise the env")
+def paramsDef = new ParametersDefinitionProperty(param)
+job.addProperty(paramsDef)
+
+jenkins.putItem(job)
+
+println "${prefix} Job '${jobName}' was created with success!"
+
+println ""
+println ""
