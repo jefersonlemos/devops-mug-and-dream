4c94e89cb09a5beb358b723651b874a7f878e18a
Anderson Babinski <andeerlb@gmail.com>
2025-08-11 23:46:38 -0300

    removed items

--- open-to-fu/data/minikube_destroy.sh
+++ open-to-fu/data/minikube_destroy.sh
@@ -3,10 +3,3 @@ set -e # stop the script if something wrong
 
 echo "Deleting Minikube..."
 minikube delete
-
-if minikube status &> /dev/null; then
-  echo "Minikube still running after delete!"
-  exit 1
-else
-  echo "Minikube successfully deleted."
-fi
