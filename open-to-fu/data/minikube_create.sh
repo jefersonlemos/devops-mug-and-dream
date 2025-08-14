87649de2f30d11e7c1c45ae56eba0d8c96252cea
Anderson Babinski <andeerlb@gmail.com>
2025-08-14 16:36:35 -0300

    some refactoring

--- open-to-fu/data/minikube_create.sh
+++ open-to-fu/data/minikube_create.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e # stop the script if something wrong
 
-echo "Starting Minikube in background..."
-minikube start --driver=docker --memory=8192 --cpus=6 --insecure-registry="registry.kube-system.svc.cluster.local:80,192.168.49.0/24"
+minikube start --driver=docker --memory=8192 --cpus=6 --insecure-registry="registry.kube-system.svc.cluster.local:80"
 minikube addons enable registry
 kubectl config use-context minikube
